1b8e77eeef9a534f1bbee19cf39bb13546751c03
SEPJason <33102830+SEPJason@users.noreply.github.com>
2017-11-08 19:35:27 +0200

    Add files via upload

--- HotelMain.java
+++ HotelMain.java
@@ -1,6 +1,9 @@
 package c9MISProject;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * 
@@ -10,18 +13,73 @@ import java.util.ArrayList;
 
 public class HotelMain {
 	
-	public static void main(String[] args){
+	public static void main(String[] args) throws FileNotFoundException{
 		//Scanner to read through text file line by line
 		// prompt user for guest or manager through system.in
 		// ask user for task to perform through system.in
 		//ask how many rooms user wants? handicap accessible? Kitchen Y/n, etc. and book room based on 
-		ArrayList<Double> rate;
-		ArrayList<Integer> bedCount;
-		ArrayList<String> hasKitchen;
-		ArrayList<String> handicapAccess;
-		ArrayList<String> isBooked; //could this be boolean?
-		ArrayList<String> needsUpkeep; //same with this, boolean?
+		ArrayList<Double> rate = new ArrayList<Double>();
+		ArrayList<Integer> bedCount = new ArrayList<Integer>();
+		ArrayList<String> hasKitchen = new ArrayList<String>();
+		ArrayList<String> handicapAccess = new ArrayList<String>();
+		ArrayList<String> isBooked = new ArrayList<String>(); //could this be boolean?
+		ArrayList<String> needsUpkeep = new ArrayList<String>(); //same with this, boolean?
+		
+		//Part 1, Adding to Array List
+		
+		String inputFileName = "hotel.txt";
+		File inputFile = new File(inputFileName);
+		Scanner ins = new Scanner(inputFile);
+		
+		while (ins.hasNextLine()) {
+			Double roomCost = ins.nextDouble();
+			Integer beds = ins.nextInt();
+			String kitchen = ins.next();
+			String handicap = ins.next();
+			String booked = ins.next();
+			String upkeep = ins.next();
 
+			rate.add(roomCost);
+			bedCount.add(beds);
+			hasKitchen.add(kitchen);
+			handicapAccess.add(handicap);
+			isBooked.add(booked);
+			needsUpkeep.add(upkeep);
+			
+	      Scanner in = new Scanner(System.in);
+	      
+	      boolean more = true;
+	      while (more)
+	      {  
+	         System.out.println("Are you a G)uest or M)anager user? (G/M)" );
+	         String cmd = in.nextLine();
+	           
+	         if (cmd.equalsIgnoreCase("G")) {
+	        	 System.out.println("Would you like to B)ook a reservation, V)iew your current reservation, C)heck in, or check O)ut?");
+		          cmd = in.nextLine();
+		          if (cmd.equalsIgnoreCase("B")){
+		        	  
+		          } else if (cmd.equalsIgnoreCase("V")){
+		        	  
+		          } else if (cmd.equalsIgnoreCase("C")){
+		        	  
+		          } else if (cmd.equalsIgnoreCase("O")){
+		        	  
+		          }
+	         }
+	         else if (cmd.equalsIgnoreCase("M"))
+	         { 
+	          System.out.println("Would you like to check O)ccupany, view G)uest count, or check what room needs U)pkeep?");
+	          cmd = in.nextLine();
+	          if (cmd.equalsIgnoreCase("O")){
+	        	  
+	          } else if (cmd.equalsIgnoreCase("G")){
+	        	  
+	          } else if (cmd.equalsIgnoreCase("U")){
+	        	  
+	          } 
+	         }
+	         in.close();
 	}
 
-}
+}}}
