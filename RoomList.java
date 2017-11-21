4822dc55642c242d8a385e878105ca71d8cff7f2
Christophertherobin <33433564+Christophertherobin@users.noreply.github.com>
2017-11-21 13:30:57 -0600

    Completed basic RoomList code
    We may want a separate method for more generalized Room List Descriptions for options. I could also change isBooked to be "hasElement" so it will test what attributes a specific index will have.

--- RoomList.java
+++ RoomList.java
@@ -7,7 +7,34 @@ package c9MISProject;
  */
 
 public class RoomList {
+	ArrayList<String> r;
+	//Creates a RoomList of i elements.
+	public  RoomList(int i){
+		r = new ArrayList<String>(i);
+	}
 	
+	//reserves room i for a guest, making the room 
+	public void reserve(int i){
+		if(isBooked(i)){
+		r.set(i, "booked");
+		}
+		if(!isBooked(i)){
+		System.out.println("That room is already booked!");
+		}
+	}
+	//displays that a guest has vacated their room, leaving it open
+	public void vacate(int i){
+		r.set(i, "open");
+	}
 	
-
+	public boolean isBooked(int i){
+		if(r.get(i).equals("booked")){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
 }
+
