c58d82031cc98d7a7e958bf3b1465c1d85d185cb
SEPJason <33102830+SEPJason@users.noreply.github.com>
2017-12-05 11:44:38 -0600

    Add files via upload

--- HotelMain.java
+++ HotelMain.java
@@ -1,5 +1,6 @@
 package c9MISProject;
 
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -7,8 +8,8 @@ import java.util.Scanner;
 
 /**
  * 
- * @author Jason Thomas
- * @author Chris Pingel
+ * @author Jason Thomas, Ben Merz, Chris Pingel, Steve Weber
+ *
  */
 
 public class HotelMain {
@@ -24,9 +25,9 @@ public class HotelMain {
 		ArrayList<Integer> bedCount = new ArrayList<Integer>();
 		ArrayList<String> hasKitchen = new ArrayList<String>();
 		ArrayList<String> handicapAccess = new ArrayList<String>();
-		ArrayList<String> isBooked = new ArrayList<String>(); // could this be boolean
+		ArrayList<String> isBooked = new ArrayList<String>(); 
 		ArrayList<String> occupied = new ArrayList<String>();
-		ArrayList<String> needsUpkeep = new ArrayList<String>(); // same with this, boolean
+		ArrayList<String> needsUpkeep = new ArrayList<String>(); 
 
 		// Part 1: adding to ArrayList
 
@@ -54,97 +55,75 @@ public class HotelMain {
 			boolean more = true;
 			int occCount = 0;
 			int guestCount = 0;
+			while(true){
 				System.out.println("Login Menu:");
 				System.out.println("Are you a Guest or Manager user?");
 				String cmd = in.nextLine();
-				while (more) {
+			
 				if (cmd.equalsIgnoreCase("Guest")) {
-					System.out.println("Main Menu:");
-					System.out.println("Would you like to B)ook a reservation, V)iew your current reservation, C)heck in, check O)ut, or Q)uit?");
-					cmd = in.nextLine();
-					if (cmd.equalsIgnoreCase("B")) {
-						GuestUser.setReservation();
+					while(true){
 						System.out.println("Main Menu:");
-						System.out.println("Would you like to check O)ccupany, view G)uest count, check what room needs U)pkeep, or Q)uit?");
+						System.out.println("Would you like to B)ook a reservation, V)iew your current reservation, C)heck in, check O)ut, or Q)uit?");
 						cmd = in.nextLine();
-						
-						//For Testing Purposes
-						// Remember that arrays start at zero, so if we want to check rates for room 1 we have to input it as "roomNum.get(1-1);," room 2 would be "roomNum.get(2-1);."
-						//for(int i = 0; i<rate.size(); i++){
-						//	System.out.println(roomNum.get(i));
-						//	System.out.println(rate.get(i));
-						//	System.out.println(bedCount.get(i));
-						//	System.out.println(hasKitchen.get(i));
-						//	System.out.println(isBooked.get(i));
-						//}
-						
-					} else if (cmd.equalsIgnoreCase("V")) {
-						GuestUser.getReservation();
-						System.out.println("Main Menu:");
-						System.out.println("Would you like to check O)ccupany, view G)uest count, check what room needs U)pkeep, or Q)uit?");
-						cmd = in.nextLine();
-					} else if (cmd.equalsIgnoreCase("C")) {
-						GuestUser.checkIn();
-						System.out.println("Main Menu:");
-						System.out.println("Would you like to check O)ccupany, view G)uest count, check what room needs U)pkeep, or Q)uit?");
-						cmd = in.nextLine();
-					} else if (cmd.equalsIgnoreCase("O")) {
-						GuestUser.checkOut();
-						System.out.println("Main Menu:");
-						System.out.println("Would you like to check O)ccupany, view G)uest count, check what room needs U)pkeep, or Q)uit?");
-						cmd = in.nextLine();
-					} else if (cmd.equalsIgnoreCase("Q")) {
-						System.out.print("Thank you for using our hotel service. Goodbye!");
-						in.close();
-						System.exit(0);
-
+						if (cmd.equalsIgnoreCase("B")) {
+							GuestUser.setReservation();	
+						} else if (cmd.equalsIgnoreCase("V")) {
+							GuestUser.getReservation();
+						} else if (cmd.equalsIgnoreCase("C")) {
+							GuestUser.checkIn();
+						} else if (cmd.equalsIgnoreCase("O")) {
+							GuestUser.checkOut();
+						} else if (cmd.equalsIgnoreCase("Q")) {
+							System.out.print("Thank you for using our hotel service. Goodbye!\n");
+							break;
+						}
 					}
 				} 
 				else if (cmd.equalsIgnoreCase("Manager")) {
-					System.out.println("Main Menu:");
-					System.out.println("Would you like to check O)ccupany, view G)uest count, check what room needs U)pkeep, or Q)uit?");
-					cmd = in.nextLine();
-					
-					if (cmd.equalsIgnoreCase("O")) {
-						for (int i = 0; i < occupied.size(); i++){
-							if(occupied.get(i) == "Yes"){
-								occCount++;
-							}
-						}
-						System.out.println("Current occupancy is: " + occCount + " guests.");
+					while(true){
 						System.out.println("Main Menu:");
 						System.out.println("Would you like to check O)ccupany, view G)uest count, check what room needs U)pkeep, or Q)uit?");
 						cmd = in.nextLine();
-
-					} else if (cmd.equalsIgnoreCase("G")) {
-						for (int i = 0; i < roomNum.size(); i++){
-							if(occupied.get(i) == "Yes"){
-								guestCount = guestCount + bedCount.get(i);
-							}
-						}
-						System.out.println("There are roughly " + guestCount + " guests currently checked in.");
-						System.out.println("Main Menu:");
-						System.out.println("Would you like to check O)ccupany, view G)uest count, check what room needs U)pkeep, or Q)uit?");
-						cmd = in.nextLine();
-					} else if (cmd.equalsIgnoreCase("U")) {
 						
-						for (int i = 0; i < needsUpkeep.size(); i++){
-							if (needsUpkeep.get(i) == "Yes"){
-								System.out.println("Room " + roomNum.get(i) + " needs upkeeping.");
-							} else if (needsUpkeep.get(i) != "Yes"){
-								System.out.println("Room " + roomNum.get(i) + " is in good condition.");
+						if (cmd.equalsIgnoreCase("O")) {
+							for (int i = 0; i < occupied.size(); i++){
+								if(occupied.get(i).equals("Yes")){
+									occCount++;
+								}
 							}
+							System.out.println("Current occupancy is: " + occCount + " guests.");
+							System.out.println("Main Menu:");
+							System.out.println("Would you like to check O)ccupany, view G)uest count, check what room needs U)pkeep, or Q)uit?");
+							cmd = in.nextLine();
+	
+						} else if (cmd.equalsIgnoreCase("G")) {
+							for (int i = 0; i < roomNum.size(); i++){
+								if(occupied.get(i).equals("Yes")){
+									guestCount = guestCount + bedCount.get(i);
+								}
+							}
+							System.out.println("There are roughly " + guestCount + " guests currently checked in.");
+							System.out.println("Main Menu:");
+							System.out.println("Would you like to check O)ccupany, view G)uest count, check what room needs U)pkeep, or Q)uit?");
+							cmd = in.nextLine();
+						} else if (cmd.equalsIgnoreCase("U")) {
+							
+							for (int i = 0; i < needsUpkeep.size(); i++){
+								if (needsUpkeep.get(i).equals("Yes")){
+									System.out.println("Room " + roomNum.get(i) + " needs upkeeping.");
+								} else {
+									System.out.println("Room " + roomNum.get(i) + " is in good condition.");
+								}
+							}
+							System.out.println("Main Menu:");
+							System.out.println("Would you like to check O)ccupany, view G)uest count, check what room needs U)pkeep, or Q)uit?");
+							cmd = in.nextLine();
+						} else if (cmd.equalsIgnoreCase("Q")) {
+							System.out.print("Thank you for using our hotel service. Goodbye!\n");
+							break;
 						}
-						System.out.println("Main Menu:");
-						System.out.println("Would you like to check O)ccupany, view G)uest count, check what room needs U)pkeep, or Q)uit?");
-						cmd = in.nextLine();
-					} else if (cmd.equalsIgnoreCase("Q")) {
-						System.out.print("Thank you for using our hotel service. Goodbye!");
-						in.close();
-						System.exit(0);
 					}
 				}
 			}
-
 		}
 	}
